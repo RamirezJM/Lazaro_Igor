918fad335f6256f36b4dc22823dd4cac58621b69
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-05-13 09:04:17 -0400

    uploading files

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const menuButton = document.querySelector('.btn-menu')
+const navMenu = document.querySelector('nav')
+
+menuButton.addEventListener("click", () =>{
+  navMenu.classList.toggle('active-nav')
+  menuButton.classList.toggle('menu-abierto')
+  menuButton.setAttribute('aria-expanded', navMenu.classList.contains('active-link'))
+})
