cef508c65ddd2f2f8f6dd0c7d5dffa584cb6fc2e
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-08-08 23:09:27 -0400

    borders, navLinks script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const menuButton = document.querySelector('.btn-menu')
 const navMenu = document.querySelector('nav')
+const navLinks = navMenu.querySelectorAll('a')
 
 menuButton.addEventListener("click", () =>{
   navMenu.classList.toggle('active-nav')
@@ -7,10 +8,29 @@ menuButton.addEventListener("click", () =>{
   menuButton.setAttribute('aria-expanded', navMenu.classList.contains('active-link'))
 })
 
-baguetteBox.run('.procesos-galeria', {
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('active-nav')
+    menuButton.classList.remove('menu-abierto')
+    menuButton.setAttribute('aria-expanded', 'false')
+  })
+})
+
+/* baguetteBox.run('.procesos-galeria', {
     buttons: true
   })
 
 baguetteBox.run('.proyectos-galeria', {
     buttons: true
   })
+ */
+if (typeof baguetteBox !== 'undefined') {
+  baguetteBox.run('.proyectos-galeria', {
+    buttons: true
+  });
+}
+if (typeof baguetteBox !== 'undefined') {
+  baguetteBox.run('.procesos-galeria', {
+    buttons: true
+  });
+}
