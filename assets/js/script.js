f50cfbc323a047af629c955635f2002d7009af9f
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-10-13 23:07:39 -0300

    marca chile, politica de privacidad

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,16 @@ if (typeof baguetteBox !== 'undefined') {
     buttons: true
   });
 }
+
+
+
+const privacyButton = document.querySelector('.privacy__btn')
+privacyButton.addEventListener('click', openModal)
+
+function openModal(){
+  const modal = document.querySelector('#privacy-modal')
+  modal.showModal()
+  modal.scrollTop = 0 
+  document.querySelector('.modal__title').focus()
+
+}
